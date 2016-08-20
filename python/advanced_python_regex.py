b4ca055b847fed265be162e0cad6e23ad3617604
RickS <rick.shapiro91@gmail.com>
2016-08-20 15:41:09 -0400

    Finished part 1

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,5 +1,6 @@
 # Part 1:
 
+# Q1 ------------------
 import pandas as pd
 import numpy as np
 df = pd.read_csv('faculty.csv')
@@ -31,3 +32,35 @@ MPH      2.0
 MS       2.0
 JD       1.0
 '''
+
+
+# Q2 ------------------
+
+# clearly a typo
+df.loc[df.title == 'Assistant Professor is Biostatistics', 'title'] = 'Assistant Professor of Biostatistics'
+df.title.unique()
+# ['Associate Professor of Biostatistics', 'Professor of Biostatistics', 'Assistant Professor of Biostatistics']
+df.title.value_counts()
+'''
+Professor of Biostatistics              13
+Associate Professor of Biostatistics    12
+Assistant Professor of Biostatistics    12
+Name: title, dtype: int64
+'''
+
+
+# Q3  ------------------
+
+# ...
+df.email 
+# i'm slightly confused by this section as it seems to be asking me about regex, 
+# but even if i weren't using pandas i'd use csv and still just refer to columns as rows
+
+emails = df.email.tolist()
+
+# Q4   ------------------
+
+all_domains = map(lambda e: e.split('@')[-1], emails)
+len(set(all_domains)) # 4
+
+
