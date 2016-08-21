2cd7286c0274ffd9beb234232177d483d4c7721e
RickS <rick.shapiro91@gmail.com>
2016-08-21 09:56:39 -0400

    Created Emails.csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,17 @@
-PLACE YOUR CODE HERE
+import re
+
+data = None
+emails = None
+domains = None
+unique_doms = None
+
+with open('faculty.csv', 'r+') as fhand:
+  data = fhand.read()
+  emails = re.findall(r',([a-z1-9]+@.+)\n?', data)
+  domains = map(lambda e: e.split('@')[1], emails)
+  unique_doms = set(domains)
+
+with open('emails.csv', 'w+') as f:
+  for e in emails:
+    f.write(e + '\n')
+
